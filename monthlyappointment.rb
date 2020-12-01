771369c6d0d432da0c7786fe3c614cf2bd3699d6
Tamara Rubilar <ta.rubilar@gmail.com>
2020-12-01 20:10:46 -0300

    MonthlyAppointment, método occurs_on responde según argumentos descritos, método to_s llamado implícito

--- monthlyappointment.rb
+++ monthlyappointment.rb
@@ -12,3 +12,14 @@ class MonthlyAppointment < Appointment #Creción clase de herencia
         super(location, purpose, hour, min)
         @day = day 
     end
+
+    def occurs_on?(day) #Método que responde según argumentos descritos 
+        @day == day
+    end
+
+    def to_s #llamado implícito
+        "Reunión mensual en #{location} sobre #{purpose} el día #{day} a las #{hour}: #{min}"
+    
+end
+
+app = MonthlyAppointment.new()
