4aaa10407e1fce3ef41f38c46a48ab22f713c33f
Tamara Rubilar <ta.rubilar@gmail.com>
2020-12-01 20:00:27 -0300

    appointment

---
+++ appointment.rb
@@ -0,0 +1,21 @@
+=begin
+    
+Estudiante: Tamara Rubilar 
+
+Desafío - Herencia y UML
+    
+=end
+
+
+require_relative 'monthlyappointment'
+
+class Appointment #Creación clase Appointment
+    attr_reader :location, :purpose, :hour, :min 
+
+    def initialize(location, purpose, hour, min)
+        @location = location
+        @purpose = purpose
+        @hour = hour
+        @min = min
+    end
+end
