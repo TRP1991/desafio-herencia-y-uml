f7d5f3a06f9987dba1f71f72a2056a4519c17a66
Tamara Rubilar <ta.rubilar@gmail.com>
2020-12-01 20:06:13 -0300

    MonthlyAppointment, creación clase de herencia e inicio constructor

--- monthlyappointment.rb
+++ monthlyappointment.rb
@@ -5,3 +5,10 @@ Estudiante: Tamara Rubilar
 Desafío - Herencia y UML
     
 =end
+
+class MonthlyAppointment < Appointment #Creción clase de herencia
+    
+    def initialize(location, purpose, hour, min) #Inicio constructor 
+        super(location, purpose, hour, min)
+        @day = day 
+    end
