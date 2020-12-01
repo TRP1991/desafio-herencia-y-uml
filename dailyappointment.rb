6a94e7aad9c3e2ce4705cc7e70bd0595e5623d4d
Tamara Rubilar <ta.rubilar@gmail.com>
2020-12-01 20:03:52 -0300

    dailyappointment, llamado implícito método to_s

--- dailyappointment.rb
+++ dailyappointment.rb
@@ -11,3 +11,8 @@ class DailyAppointment < Appointment #Creación de clase de herencia
     def occurs_on?(hour,min)
         @hour == hour && @min == min
     end
+
+    def to_s
+        "Reunión diaria en #{location} sobre #{purpose} a las #{hour}: #{min}"
+    end
+end
